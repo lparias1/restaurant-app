6e1e4d06cef7cdb8119f37a4c3062e3f3db840ff
Lidier Andres Parias Ayala <lidierandres1994@gmail.com>
2025-08-13 16:20:47 -0500

    commit 5

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/restaurant-app",
+  base: "https://github.com/lparias1/restaurant-app",
   plugins: [react()],
 })
